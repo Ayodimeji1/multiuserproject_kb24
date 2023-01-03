90023a21ffbc83cae4efffb5d694e34405dbd7f0
Ayodimeji Kadri <ayodimejia1@gmail.com>
2023-01-03 17:26:47 +0100

    dockerizinf app

--- backend/carlender/settings.py
+++ backend/carlender/settings.py
@@ -88,8 +88,6 @@ WSGI_APPLICATION = 'carlender.wsgi.application'
 
 DATABASES = {
     'default': {
-        # 'ENGINE': 'django.db.backends.sqlite3',
-        # 'NAME': BASE_DIR / 'db.sqlite3',
         'ENGINE': 'django.db.backends.postgresql',
         'NAME': 'lender',
         'USER': 'postgres',
